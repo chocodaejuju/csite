cfe02c5d6edff7752b7a1b9b25ce54cac317043b
kimjuyeon <happykim0709@gmail.com>
2023-10-28 17:31:01 +0900

    d

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -10,11 +10,12 @@ environ.Env.read_env(BASE_DIR / '.env')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('DB_HOST'),
-        'PORT': '5432',
+        'PORT': '3306',
+        "OPTIONS": {"charset": "utf8mb4"},
     }
 }
